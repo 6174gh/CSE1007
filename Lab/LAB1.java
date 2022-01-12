5b1eb8b98def44add056170f63abfab0e13fc94a
6174g <97239948+6174g@users.noreply.github.com>
2022-01-12 21:51:42 +0530

    120122

--- Lab/LAB1.java
+++ Lab/LAB1.java
@@ -1,7 +1,12 @@
-package Lab;
-
-public class LAB1 {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+import java.lang.Math;
+import java.util.Scanner;
+public class main{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        float a = scanner.nextInt();
+        scanner.close();
+        a *= 10;
+        int u = Math.round(a);
+        System.out.println(u%10);
     }
 }
